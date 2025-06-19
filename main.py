aeac2d443309e7280da785e1a49f253ef59e025c
Dekoder-py <kyleyesmariam@icloud.com>
2025-06-20 11:52:39 +1200

    add caption to window, comments, change quiting and display updating

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
+import sys
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-running = True
+pygame.init()  # setup pygame
 
-while running:
+pygame.display.set_caption("2D Platformer")  # set window name
+screen = pygame.display.set_mode((640, 480))  # create window
+clock = pygame.time.Clock()  # clock for frame rate
+
+while True:  # runs until quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()  # exit pygame
+            sys.exit()  # end program
 
     screen.fill("#89b4fa")
 
-    pygame.display.flip()
+    pygame.display.update()
     clock.tick(60)
