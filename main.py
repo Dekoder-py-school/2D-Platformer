1e8770d5fc1ea214fc79cf35a1270645345ac219
Dekoder-py <kyleyesmariam@icloud.com>
2025-06-20 14:17:04 +1200

    add movement with WASD or arrow keys.

--- main.py
+++ main.py
@@ -10,15 +10,40 @@ class Game:
         self.clock = pygame.time.Clock()  # clock for frame rate
         self.sprite = pygame.image.load("./assets/sprite.png")
 
+        self.sprite_pos = [160, 260]
+        self.y_movement = [False, False]
+        self.x_movement = [False, False]
+
     def run(self):
         while True:  # runs until quit
+            self.screen.fill("#89b4fa")
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # exit pygame
                     sys.exit()  # end program
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.y_movement[0] = True
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.y_movement[1] = True
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.x_movement[0] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.x_movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.y_movement[0] = False
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.y_movement[1] = False
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.x_movement[0] = False
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.x_movement[1] = False
 
-            self.screen.fill("#89b4fa")
-            self.screen.blit(self.sprite, (100, 200))
+            self.sprite_pos[1] += (self.y_movement[1] - self.y_movement[0]) * 5
+            self.sprite_pos[0] += (self.x_movement[1] - self.x_movement[0]) * 5
+            self.screen.blit(self.sprite, self.sprite_pos)
 
             pygame.display.update()
             self.clock.tick(60)
